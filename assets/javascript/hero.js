0fa4510f769dd0da07e4aa7988a82c79af3efdfa
samuelmormin <samuel.mormin@hetic.net>
2017-11-22 00:41:57 +0100

    :sparkles: Accelerometer handle added for hero section

--- assets/javascript/hero.js
+++ assets/javascript/hero.js
@@ -10,3 +10,32 @@ function moveBackground(e) {
   let y = e.clientY
   hero_image.style.transform = 'translateX(' + x/100 + '%) ' + 'translateY(' + y/40 + '%)'
 }
+
+// Accelerometer handle
+// Get elements to move with device orientation
+const hero_image_container = document.querySelector('.hero__containerImages')
+
+let maxX = hero_image_container.clientWidth  - hero_image.clientWidth
+let maxY = hero_image_container.clientHeight - hero_image.clientHeight
+
+function handleAccelerometer(event) {
+  let x = event.beta  // In degree in the range [-180,180]
+  let y = event.gamma // In degree in the range [-90,90]
+
+  // Constrain the x value to the range [-90,90] to don't have the device upside down
+  if (x >  90) { x =  90}
+  if (x < -90) { x = -90}
+
+  // To make computation easier, shift the range of 
+  // x and y to [0,180]
+  x += 90
+  y += 90
+
+  // Define half of the width and height of the image to move
+  // It center the positioning point to the center of the ball
+  hero_image.style.top  = (maxX * x / 180 - 500) + 'px'
+  hero_image.style.left = (maxY * y / 180 - 250) + 'px'
+}
+
+// Listen to deviceOrientation event
+window.addEventListener('deviceorientation', (event) => handleAccelerometer(event))
