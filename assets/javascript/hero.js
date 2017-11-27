011444f8656ffaa5900bc36e8c083e9b2761c8c7
samuelmormin <samuel.mormin@hetic.net>
2017-11-27 02:24:03 +0100

    :bug: Hero section mouse move event fixed

--- assets/javascript/hero.js
+++ assets/javascript/hero.js
@@ -1,16 +1,19 @@
 export default class Hero {
   // Translate background on mousemove
   translate() {
+    const hero = document.querySelector('.hero')    
     const hero_image = document.querySelector('.hero__image--background')
-
+    const hero_eyes = document.querySelector('.hero__image--eyes')
+    
     function moveBackground(e) {
       let x = e.clientX
       let y = e.clientY
-      hero_image.style.transform = `translate(${x}/100%, ${y} /40%)`
+      hero_image.style.transform = `translate(${x/30}px,${y/10}px)`
+      hero_eyes.style.transform = `translate(${x/130}px,${y/40}px)`
     }
 
     // Listen to mousemove event and run moveBackground function
-    window.addEventListener('mousemove', (e) => moveBackground(e))
+    hero.addEventListener('mousemove', (e) => moveBackground(e))
   }
 
   // Fallback on mobile using accelerometer
