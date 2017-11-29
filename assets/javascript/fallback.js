0ae2b0bdf7f3f5e01cb5f2251783617b80b73487
keellyp <kelly.phan@hetic.net>
2017-11-29 17:05:49 +0100

    :fire: Remove files
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- assets/javascript/fallback.js
+++
@@ -1,7 +0,0 @@
-export default class FallbackBrowser {
-  constructor(){
-    this.chrome   = navigator.userAgent.indexOf('Chrome') > -1
-    this.firefox  = navigator.userAgent.indexOf('Firefox') > -1
-    this.safari   = navigator.userAgent.indexOf('Safari') > -1
-  }
-}
