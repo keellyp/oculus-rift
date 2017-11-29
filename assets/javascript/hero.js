a75c4f3d04956d443d18888d396dc390cf9657c4
samuelmormin <samuel.mormin@hetic.net>
2017-11-29 23:42:51 +0100

    :bento:Js code deleted

--- assets/javascript/hero.js
+++ assets/javascript/hero.js
@@ -16,40 +16,4 @@ export default class Hero {
     hero.addEventListener('mousemove', (e) => moveBackground(e))
   }
 
-  // Fallback on mobile using accelerometer
-  accelerometer() {
-    const hero_image = document.querySelector('.hero__image--background')
-    const hero_image_container = document.querySelector('.hero__containerImages')
-
-    let maxX = hero_image_container.clientWidth - hero_image.clientWidth
-    let maxY = hero_image_container.clientHeight - hero_image.clientHeight
-
-    function handleAccelerometer(event) {
-      let x = event.beta // In degree in the range [-180,180]
-      let y = event.gamma // In degree in the range [-90,90]
-
-      // Constrain the x value to the range [-90,90] to don't have the device upside down
-      if (x > 90) {
-        x = 90
-      }
-      if (x < -90) {
-        x = -90
-      }
-
-      // To make computation easier, shift the range of 
-      // x and y to [0,180]
-      x += 90
-      y += 90
-
-      // Define half of the width and height of the image to move
-      // It center the positioning point to the center of the ball
-      // hero_image.style.top = `${maxX * x / 180 - 500}px`
-      // hero_image.style.left = `${maxY * y / 180 - 250}px`
-      console.log(`(${maxY * y / 180 - 250})px`)
-      console.log(`(${maxX * x / 180 - 500})px`)
-    }
-
-    // Listen to deviceOrientation event
-    window.addEventListener('deviceorientation', (e) => {handleAccelerometer(e)} )
-  }
 }
