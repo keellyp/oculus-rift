73e1263a8587a6f30aa68c1d340a3c34700bc306
keellyp <kelly.phan@hetic.net>
2017-12-05 12:43:38 +0100

    :sparkles: Deploy new gulpfile
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- gulpfile.js
+++ gulpfile.js
@@ -1,264 +1,153 @@
+// Get dependencies 
+const gulp    = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  browserify  = require('browserify'),
+  babelify    = require('babelify'),
+  buffer      = require('vinyl-buffer'),
+  source      = require('vinyl-source-stream'),
+  env         = require('babel-preset-env')
+
+// Get all gulp dependencies 
+const plugin = require('gulp-load-plugins')();
+
 // Variables
 const config = {
-  styles: 'assets/styles/',
-  js: 'assets/javascript/',
   assets: 'assets/',
   dist: 'dist/',
   isProd: process.env.NODE_ENV === 'production'
 }
 
-/*eslint-disable*/
-const gulp          = require('gulp'),
-  // Tools dependencies
-  gulp_util         = require('gulp-util'),
-  del               = require('del'),
-  gulp_rename       = require('gulp-rename'),
-  gulp_plumber      = require('gulp-plumber'),
-  gulp_notify       = require('gulp-notify'),
-  gulp_sourcemaps   = require('gulp-sourcemaps'),
-  browserSync       = require('browser-sync').create(),
-  gulp_fileinclude  = require('gulp-file-include'),
-  // Image depedency
-  gulp_imagemin     = require('gulp-imagemin'),
-  // Style dependencies
-  gulp_sass         = require('gulp-sass'),
-  gulp_autoprefixer = require('gulp-autoprefixer'),
-  gulp_cssnano      = require('gulp-cssnano'),
-  gulp_concatcss    = require('gulp-concat-css'),
-  // Javascript dependencies
-  browserify        = require('browserify'),
-  babelify          = require('babelify'),
-  buffer            = require('vinyl-buffer'),
-  source            = require('vinyl-source-stream'),
-  es2015            = require('babel-preset-es2015'),
-  gulp_uglify       = require('gulp-uglify')
-/*es-lint enable */
-
-// BrowserSync http://localhost:3000/ : static server + watching HTML, SCSS, JS files
-gulp.task('serve', ['style'], () => {
+// BrowserSync http://localhost:3000/ 
+gulp.task('browserSync', () => {
   browserSync.init({
-    server: 'dist/'
+    server: config.dist
   })
-  gulp.watch(`${config.dist}**/*.html`).on('change', browserSync.reload)
-  gulp.watch(`${config.styles}*.scss`, ['style'])
-  gulp.watch(`${config.js}*.js`, ['check-scripts'])
 })
-
-// Ensure that 'javascript' task is complete before reload
-gulp.task('check-scripts', ['javascript'], (done) => {
-  browserSync.reload()
-  done()
-})
-
-// Default task
-gulp.task('default', ['serve', 'watch'], () => {})
-
-// Build task
-gulp.task('build', ['clean', 'fileinclude', 'style', 'javascript', 'aframe', 'fonts', 'videos', 'images'], () => {})
 
 // Clean dist 
 gulp.task('clean', () => {
-  del([config.dist], {
-    force: true,
-    dryRun: true
-  })
+  return gulp.src(`${config.dist}**/*`, { read: false })
+    .pipe(plugin.rm())
 })
 
-// CSS function
+// CSS function : Handle Sass, autoprefix, minify, sourcemaps
 gulp.task('style', () => {
-  return gulp.src(`${config.styles}main.scss`)
-    .pipe(gulp_plumber({
-      errorHandler: gulp_notify.onError('SASS Error <%= error.message %>')
+  gulp
+    .src(`${config.assets}/scss/*.scss`)
+    .pipe(plugin.plumber({
+      errorHandler: plugin.notify.onError('SASS Error <%= error.message %>')
     }))
-    .pipe(!config.isProd ? gulp_sourcemaps.init() : gulp_util.noop())
-    .pipe(gulp_sass({
+    .pipe(!config.isProd ? plugin.sourcemaps.init() : plugin.util.noop())
+    .pipe(plugin.sass({
       outputStyle: 'compressed'
-    }).on('error', gulp_sass.logError))
-    .pipe(gulp_autoprefixer({
+    })
+    .on('error', plugin.sass.logError))
+    .pipe(plugin.autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(config.isProd ? gulp_cssnano() : gulp_util.noop())
-    .pipe(!config.isProd ? gulp_sourcemaps.write() : gulp_util.noop())
-    .pipe(gulp_rename('style.min.css'))
+    .pipe(plugin.csscomb())
+    .pipe(config.isProd ? plugin.cssnano() : plugin.util.noop())
+    .pipe(!config.isProd ? plugin.sourcemaps.write() : plugin.util.noop())
+    .pipe(plugin.rename('style.min.css'))
     .pipe(gulp.dest(`${config.dist}css`))
     .pipe(browserSync.stream())
-    .pipe(!config.isProd ? gulp_notify('SCSS for dev done') : gulp_util.noop())
+    plugin.util.log(plugin.util.colors.green('Style is done'))
 })
 
-// JS function
+// JS function : Browserify, Minify, Sourcemaps
 gulp.task('javascript', () => {
-  return (browserify(`${config.js}script.js`, {
-      debug: true
-    }).transform(babelify, {
-      presets: [es2015]
-    }).bundle())
-    .on('error', gulp_notify.onError(function (error) {
-      return 'Message to the notifier: ' + error.message
-    }))
-    .pipe(source('script.js'))
-    .pipe(buffer())
-    .pipe(gulp_sourcemaps.init())
-    .pipe(config.isProd ? gulp_uglify() : gulp_util.noop())
-    .pipe(gulp_sourcemaps.write())
-    .pipe(gulp_rename('script.min.js'))
-    .pipe(gulp.dest(`${config.dist}js`))
-    .pipe(!config.isProd ? gulp_notify('JS for dev done') : gulp_util.noop())
+  let bundler = browserify({
+    entries: `${config.assets}/javascript/app.js`,
+    debug: true
+  }).transform(babelify, {presets: [env]});
+  
+  return bundler.bundle()
+  .pipe(source('app.js'))
+  .pipe(buffer())
+  .pipe(plugin.plumber({errorHandler: plugin.notify.onError("Error: <%= error.message %>")}))
+  .pipe(plugin.sourcemaps.init({loadMaps: true}))
+  .pipe(config.isProd ? plugin.streamify(plugin.uglify()) : plugin.util.noop())
+  .pipe(plugin.rename('app.min.js'))
+  .pipe(gulp.dest(`${config.dist}js`))
+  .pipe(browserSync.stream())
+  plugin.util.log(plugin.util.colors.green('JS is done'))    
 })
 
-// JS function
-gulp.task('aframe', () => {
-  return (browserify(`${config.js}aframe.js`, {
-      debug: true
-    }).transform(babelify, {
-      presets: [es2015]
-    }).bundle())
-    .on('error', gulp_notify.onError(function (error) {
-      return 'Message to the notifier: ' + error.message
-    }))
-    .pipe(source('aframe.js'))
-    .pipe(buffer())
-    .pipe(gulp_sourcemaps.init())
-    .pipe(config.isProd ? gulp_uglify() : gulp_util.noop())
-    .pipe(gulp_sourcemaps.write())
-    .pipe(gulp_rename('aframe.min.js'))
-    .pipe(gulp.dest(`${config.dist}js`))
-    .pipe(!config.isProd ? gulp_notify('JS for dev done') : gulp_util.noop())
-})
+// Create different size for images 
+gulp.task('srcset', () => {
+  return gulp.src(`${config.assets}images/src/*.{png, jpg}`)
+    .pipe(plugin.responsive({
+      '*': [
+        { width: 350, rename: { suffix: '@350w' }, },
+        { width: 560, rename: { suffix: '@560w' }, },
+        { width: 720, rename: { suffix: '@720w' }, },
+        { width: 1280, rename: { suffix: '@1280w' }, },
+        { width: 1920, rename: { suffix: '@1920w' }, },
+        { rename: { suffix: '-full' }, }], 
+      }, {
+      quality: 70,
+      progressive: true,
+      withMetadata: false,
+      }))
+    .pipe(gulp.dest(`${config.assets}images`));
+});
 
-
-// Minifies images
+// Image optimisation
 gulp.task('images', () => {
-  return gulp.src(`${config.assets}images/*`)
-    .pipe(config.isProd ? gulp_imagemin() : gulp_util.noop())
+  gulp
+    .src(`${config.assets}images/*.{png, jpg, svg, gif}`)
+    .pipe(!config.isProd ? plugin.imagemin([
+      plugin.imagemin.gifsicle({interlaced: true}),
+      plugin.imagemin.jpegtran({progressive: true}),
+      plugin.imagemin.optipng({optimizationLevel: 5}),
+      plugin.imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ], {
+      verbose: true
+    }) : plugin.util.noop())
     .pipe(gulp.dest(`${config.dist}img`))
-})
-
-// Replace videos into dist folder
-gulp.task('videos', () => {
-  return gulp.src(`${config.assets}videos/*`)
-    .pipe(gulp.dest(`${config.dist}videos`))
+    .pipe(browserSync.stream())
+    plugin.util.log(plugin.util.colors.green('Images is done'))    
 })
 
 // Replace font into dist folder
 gulp.task('fonts', () => {
-  return gulp.src(`${config.assets}fonts/*`)
+  gulp
+    .src(`${config.assets}fonts/*`)
     .pipe(gulp.dest(`${config.dist}fonts`))
+    .pipe(browserSync.stream())
+    plugin.util.log(plugin.util.colors.green('Fonts has been move'))
 })
 
 // Include HTML files into dist folder under the name of index.html 
 gulp.task('fileinclude', function () {
-  gulp.src(`${config.assets}/index.html`)
-    .pipe(gulp_fileinclude({
+  gulp
+    .src(`${config.assets}/index.html`)
+    .pipe(plugin.fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
     .pipe(gulp.dest(`${config.dist}`))
+    .pipe(browserSync.stream())
+    plugin.util.log(plugin.util.colors.green('File has been included'))    
 })
-
-// Favicon generator
-
-var realFavicon = require ('gulp-real-favicon');
-var fs = require('fs');
-
-// File where the favicon markups are stored
-var FAVICON_DATA_FILE = 'faviconData.json';
-
-// Generate the icons. This task takes a few seconds to complete.
-// You should run it at least once to create the icons. Then,
-// you should run it whenever RealFaviconGenerator updates its
-// package (see the check-for-favicon-update task below).
-gulp.task('generate-favicon', function(done) {
-	realFavicon.generateFavicon({
-		masterPicture: 'assets/images/master_favicon.png',
-		dest: 'dist/img',
-		iconsPath: '/',
-		design: {
-			ios: {
-				pictureAspect: 'backgroundAndMargin',
-				backgroundColor: '#ffffff',
-				margin: '35%',
-				assets: {
-					ios6AndPriorIcons: false,
-					ios7AndLaterIcons: false,
-					precomposedIcons: false,
-					declareOnlyDefaultIcon: true
-				}
-			},
-			desktopBrowser: {},
-			windows: {
-				pictureAspect: 'whiteSilhouette',
-				backgroundColor: '#2b5797',
-				onConflict: 'override',
-				assets: {
-					windows80Ie10Tile: false,
-					windows10Ie11EdgeTiles: {
-						small: false,
-						medium: true,
-						big: false,
-						rectangle: false
-					}
-				}
-			},
-			androidChrome: {
-				pictureAspect: 'shadow',
-				themeColor: '#ffffff',
-				manifest: {
-					name: 'Oculus',
-					display: 'standalone',
-					orientation: 'notSet',
-					onConflict: 'override',
-					declared: true
-				},
-				assets: {
-					legacyIcon: false,
-					lowResolutionIcons: false
-				}
-			},
-			safariPinnedTab: {
-				pictureAspect: 'silhouette',
-				themeColor: '#000000'
-			}
-		},
-		settings: {
-			scalingAlgorithm: 'Mitchell',
-			errorOnImageTooSmall: false
-		},
-		markupFile: FAVICON_DATA_FILE
-	}, function() {
-		done();
-	});
-});
-
-// Inject the favicon markups in your HTML pages. You should run
-// this task whenever you modify a page. You can keep this task
-// as is or refactor your existing HTML pipeline.
-gulp.task('inject-favicon-markups', function() {
-	return gulp.src([ 'dist/index.html' ])
-		.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
-		.pipe(gulp.dest('dist'));
-});
-
-// Check for updates on RealFaviconGenerator (think: Apple has just
-// released a new Touch icon along with the latest version of iOS).
-// Run this task from time to time. Ideally, make it part of your
-// continuous integration system.
-gulp.task('check-for-favicon-update', function(done) {
-	var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
-	realFavicon.checkForUpdates(currentVersion, function(err) {
-		if (err) {
-			throw err;
-		}
-	});
-});
 
 // Watch all my task
-gulp.task('watch', ['fileinclude', 'style', 'javascript', 'aframe', 'fonts', 'images'], () => {
+gulp.task('watch', ['fileinclude', 'style', 'javascript', 'fonts', 'images'], () => {
   gulp.watch(`${config.assets}**/*.html`, ['fileinclude'])
-  gulp.watch(`${config.styles}**/*.scss`, ['style'])
-  gulp.watch(`${config.js}**/*.js`, ['javascript'])
-  gulp.watch(`${config.js}**/.js`, ['aframe'])
-  gulp.watch(`${config.assets}images/*`, ['images'])
-  gulp.watch(`${config.assets}videos/*`, ['videos'])
+  gulp.watch(`${config.assets}scss/**/*.scss`, ['style'])
+  gulp.watch(`${config.assets}javascript/**/*.js`, ['javascript'])
+  gulp.watch(`${config.assets}images/**/*`, ['images'])
   gulp.watch(`${config.assets}fonts/*`, ['fonts'])
 })
+
+// Default task 
+gulp.task('default', ['browserSync', 'watch'], () => {})
+
+// Build task
+gulp.task('build', ['clean', 'fileinclude', 'style', 'javascript', 'fonts', 'images'], () => {})
