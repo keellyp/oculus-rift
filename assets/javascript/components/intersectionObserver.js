496d4e449912fa8e72d935bd7ff1edf93c877666
keellyp <kelly.phan@hetic.net>
2017-12-13 00:08:16 +0100

    :hankey: Should clean this code tomorrow
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- assets/javascript/components/intersectionObserver.js
+++ assets/javascript/components/intersectionObserver.js
@@ -1,24 +1,19 @@
-const revealOnScroll = (element, callback) => {
-  const observer = new IntersectionObserver( observables => {
-    observables.forEach(observable => {
-      if (observable.intersectionRatio <= 0.3) {
-        return
-      }
-      else {
-        callback()
-        console.log('callback')
-        observer.unobserve(observable.target)
-      }
-    })
-  }, {
-    threshold: [0.3]
+const observer = new IntersectionObserver(observables => {
+  observables.forEach(observable => {
+    // L'élément devient visible
+    if (observable.intersectionRatio > 0.5) {
+      observable.target.classList.remove('not-visible')
+      observer.unobserve(observable.target)
+      console.log('visible')
+    }
   })
-}
+}, {
+  threshold: [0.5]
+})
 
-export {revealOnScroll}
-
-// let items = document.querySelectorAll('.images')
-// items.forEach(item => { 
-//   item.classList.add('not-visible')
-//   observer.observe(item)
-// })
+// On observe nos éléments
+let items = document.querySelectorAll('.images')
+items.forEach(item => {
+  item.classList.add('not-visible')
+  observer.observe(item)
+})
--- assets/javascript/sections/magic.js
+++ assets/javascript/sections/magic.js
@@ -1,6 +1,6 @@
 import revealOnScroll from '../components/intersectionObserver'
 
-const $section = document.querySelector('.magic')
+const $section = document.querySelector('.magic__image')
 
 revealOnScroll($section, () => {
   console.log('ok')
