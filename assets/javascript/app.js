f8bfc35fe6e839ab0cdc3d4ea94ccc64ca453c17
keellyp <kelly.phan@hetic.net>
2017-12-31 00:39:26 +0100

    :art: Add data-load for axios
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 import 'intersection-observer'
+import './sections/loadingPage'
 import './sections/hero'
 import './sections/magic'
 import './sections/immersiveTurtle'
@@ -6,60 +7,3 @@ import './sections/gamepad'
 import './sections/conception'
 import './sections/accessories'
 import './sections/burgerMenu'
-
-
-// Intersection observer
-
-// // Scroll.js
-// // import RevealOnSroll from './scroll'
-// const reveal = new RevealOnSroll()
-// reveal.magic()
-// reveal.gesture()
-// reveal.conception()
-
-// // Immersive.js
-// import Immersive from './immersive'
-// const immersive = new Immersive()
-// immersive.translate()
-
-// // Landing
-// import OculusAnim from './landing'
-// const oculus = new OculusAnim()
-// oculus.autoplayAnim()
-
-// //Loading 
-// import Loader from './loading'
-// const loadingPage = new Loader()
-
-// loadingPage.on('progress', value => {
-//   document.querySelector('.loading__percent').innerHTML = `${value}%`
-//   document.querySelector('.loading__progress--fill').style.transform = `scaleX(${value/100})`
-//   const caption = document.querySelector('.loading__caption')
-//   switch (value) {
-//   case 0:
-//     caption.innerHTML = 'Génération de l\'espace virtuel...'
-//     break
-//   case 40:
-//     caption.innerHTML = 'Ajout de l\'environnement et des décors'
-//     break
-//   case 80:
-//     caption.innerHTML = 'C\'est bientôt terminé'
-//     break
-//   case 100:
-//     caption.innerHTML = 'Bienvenue'
-//     break
-//   default:
-//     break
-//   }
-// })
-
-// loadingPage.on('complete', () => {
-//   const loadingEl = document.querySelector('.loading')
-//   setTimeout(() => {
-//     loadingEl.classList.add('loading--complete')
-//   }, 1000)
-//   setTimeout(() => {
-//     loadingEl.remove()
-//   }, 1400)
-
-// })
---
+++ assets/javascript/components/loading.js
@@ -0,0 +1,86 @@
+import axios from 'axios'
+
+export default class Loader {
+  // Create every variable needed
+  constructor() {
+    this.srcElements = [].slice.call(document.querySelectorAll('[data-load]'))
+    this.eventsList = ['progress', 'complete']
+    this.progressEvents = []
+    this.completeEvents = []
+    this.totalProgress = 0
+    this.requests = []
+    this.init()
+  }
+
+  // Init elements
+  init() {
+    this.srcElements.forEach(element => {
+      this.requests.push({
+        element,
+        type: 'data-src',
+        typeSrc: 'src',
+        progress: 0
+      })
+    })
+
+    // For each element, calculate the loaded percentage
+    this.requests.forEach((request, index) => {
+      axios.get(request.element.getAttribute(request.type) || request.element.getAttribute(request.typeSrc), {
+        onDownloadProgress: e => {
+          const percent = Math.floor(e.loaded / e.total * 100)
+          this.updateRequest(index, percent)
+        }
+      })
+    })
+  }
+
+  // If all element are loaded, it call the callCompleteEvents() 
+  updateRequest(index, percent) {
+    this.requests[index].progress = percent
+    const total = this.requests.reduce((value, request) => value + request.progress, 0)
+
+    this.totalProgress = Math.floor(total / this.requests.length)
+
+    if (this.totalProgress === 100) {
+      this.callProgress()
+      this.callComplete()
+    } else {
+      this.callProgress()
+    }
+  }
+
+  callProgress() {
+    this.progressEvents.forEach(({
+      callback
+    }) => {
+      callback(this.totalProgress)
+    })
+  }
+
+  callComplete() {
+    this.completeEvents.forEach(({
+      callback
+    }) => {
+      callback()
+    })
+  }
+
+  on(event, callback) {
+    switch (event) {
+      case 'progress':
+        this.progressEvents.push({
+          callback
+        })
+        break
+
+      case 'complete':
+        this.completeEvents.push({
+          callback
+        })
+        break
+
+      default:
+        break
+    }
+  }
+}
