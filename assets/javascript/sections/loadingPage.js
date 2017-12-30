34b031cd2f7f96368426e29c3bfe174d2619c33d
keellyp <kelly.phan@hetic.net>
2017-12-31 00:37:36 +0100

    :zap: Improve experience by adding a loading page
    Signed-off-by: keellyp <kelly.phan@hetic.net>

---
+++ assets/javascript/sections/loadingPage.js
@@ -0,0 +1,36 @@
+//Loading 
+import Loader from '../components/loading'
+const loadingPage = new Loader()
+
+loadingPage.on('progress', value => {
+  document.querySelector('.loading__percent').innerHTML = `${value}%`
+  document.querySelector('.loading__progress--fill').style.transform = `scaleX(${value/100})`
+  const caption = document.querySelector('.loading__caption')
+  switch (value) {
+  case 0:
+    caption.innerHTML = 'Génération de l\'espace virtuel...'
+    break
+  case 40:
+    caption.innerHTML = 'Ajout de l\'environnement et des décors'
+    break
+  case 80:
+    caption.innerHTML = 'C\'est bientôt terminé'
+    break
+  case 100:
+    caption.innerHTML = 'Bienvenue'
+    break
+  default:
+    break
+  }
+})
+
+loadingPage.on('complete', () => {
+  const loadingEl = document.querySelector('.loading')
+  setTimeout(() => {
+    loadingEl.classList.add('loading--complete')
+  }, 1000)
+  setTimeout(() => {
+    loadingEl.remove()
+  }, 1400)
+
+})
