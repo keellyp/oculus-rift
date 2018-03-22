ed02f4d58da3d6fb31dc5c4bed6281622535dad6
Kelly Phan <keelly.p@gmail.com>
2018-03-22 17:40:09 +0100

    💄 Fix loader's color

--- assets/javascript/sections/loadingPage.js
+++ assets/javascript/sections/loadingPage.js
@@ -4,23 +4,23 @@ const loadingPage = new Loader()
 
 loadingPage.on('progress', value => {
   document.querySelector('.loading__percent').innerHTML = `${value}%`
-  document.querySelector('.loading__progress--fill').style.transform = `scaleX(${value/100})`
+  document.querySelector('.loading__progress--fill').style.transform = `scaleX(${value / 100})`
   const caption = document.querySelector('.loading__caption')
   switch (value) {
-  case 0:
-    caption.innerHTML = 'Génération de l\'espace virtuel...'
-    break
-  case 40:
-    caption.innerHTML = 'Ajout de l\'environnement et des décors'
-    break
-  case 80:
-    caption.innerHTML = 'C\'est bientôt terminé'
-    break
-  case 100:
-    caption.innerHTML = 'Bienvenue'
-    break
-  default:
-    break
+    case 0:
+      caption.innerHTML = 'Génération de l\'espace virtuel...'
+      break
+    case 40:
+      caption.innerHTML = 'Ajout de l\'environnement et des décors'
+      break
+    case 80:
+      caption.innerHTML = 'Le chargement est bientôt terminé'
+      break
+    case 100:
+      caption.innerHTML = 'Tout est prêt, bienvenue'
+      break
+    default:
+      break
   }
 })
 
